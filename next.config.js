c7aaa86aae9c52a4da0f3830e6b418cd902e91c3
Steve <stevezuluu@gmail.com>
2026-03-23 09:37:25 +0200

    optimize build tracing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { domains: ['i.ibb.co'] },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        '**/@swc/core-*/**',
+        '**/node_modules/@esbuild/**',
+        '**/node_modules/webpack/**',
+      ],
+    },
   },
 }
 
